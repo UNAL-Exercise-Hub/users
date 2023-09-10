c74ac5d81ea0a7cb15853c1dd75cc482cba4c468
Julián Camilo Alfonso Carrillo <julcalfonsocar@unal.edu.co>
2023-09-10 03:41:50 -0500

    Commit inicial

---
+++ unworkout_users_ms/src/main/java/com/unworkout_users/UnworkoutUsersMsApplication.java
@@ -0,0 +1,13 @@
+package com.unworkout_users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnworkoutUsersMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnworkoutUsersMsApplication.class, args);
+	}
+
+}
---
+++ unworkout_users_ms/src/test/java/com/unworkout_users/UnworkoutUsersMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unworkout_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnworkoutUsersMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
